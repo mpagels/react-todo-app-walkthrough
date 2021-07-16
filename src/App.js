237fa634c9e0a4de6c7a449d1ea5dec6af702227
Martin Pagels <office@martinpagels.de>
2021-07-17 00:13:17 +0200

    Add header
    - inside header are
    - a form with an input and a button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Martins ToDo App</h1>
+        <form>
+          <input
+            id="addTodo"
+            name="addTodo"
+            type="text"
+            placeholder="Buy Milk"
+          ></input>
+          <button>Add Todo</button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
