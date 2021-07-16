49faaffe940e7998dd7057be2ed47eb62193fea6
Martin Pagels <office@martinpagels.de>
2021-07-17 00:43:45 +0200

    Make ToDoList more semantic
    - Change section to li and wrap ul element around

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function ToDoList() {
   return (
-    <>
-      <section className="ToDo">
+    <ul>
+      <li className="ToDo">
         <button className="ToDo__btn delete">X</button>
         <p>This is a ToDo</p>
         <button className="ToDo__btn pending">Pending</button>
-      </section>
-      <section className="ToDo">
+      </li>
+      <li className="ToDo">
         <button className="ToDo__btn delete">X</button>
         <p>This is a another ToDo</p>
         <button className="ToDo__btn done">Done</button>
-      </section>
-    </>
+      </li>
+    </ul>
   );
 }
