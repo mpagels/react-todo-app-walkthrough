b60d033bb1a956c02dacd0835add09e3e582ea9f
Martin Pagels <office@martinpagels.de>
2021-07-17 00:40:49 +0200

    Refactor header html to Header component
    - Create Header.js, move HTML, import Header component in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Martins ToDo App</h1>
-        <form>
-          <input
-            id="addTodo"
-            name="addTodo"
-            type="text"
-            placeholder="Buy Milk"
-          ></input>
-          <button>Add Todo</button>
-        </form>
-      </header>
+      <Header />
       <main>
         <section className="ToDo">
           <button className="ToDo__btn delete">X</button>
