db23443f8e72b0ef20e0b57c0590bf0f994ab9bc
Martin Pagels <office@martinpagels.de>
2021-07-17 00:55:44 +0200

    Render todos through .map in ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,16 +3,21 @@ import React from "react";
 export default function ToDoList({ todos }) {
   return (
     <ul>
-      <li className="ToDo">
-        <button className="ToDo__btn delete">X</button>
-        <p>This is a ToDo</p>
-        <button className="ToDo__btn pending">Pending</button>
-      </li>
-      <li className="ToDo">
-        <button className="ToDo__btn delete">X</button>
-        <p>This is a another ToDo</p>
-        <button className="ToDo__btn done">Done</button>
-      </li>
+      {todos.map((todo) => {
+        return (
+          <li key={todo.todo} className="ToDo">
+            <button className="ToDo__btn delete">X</button>
+            <p>{todo.todo}</p>
+            <button
+              className={`ToDo__btn ${
+                todo.status === "Pending" ? "pending" : "done"
+              }`}
+            >
+              {todo.status}
+            </button>
+          </li>
+        );
+      })}
     </ul>
   );
 }
