5b2fd066ea04e8269fc41e405def5a96fa5308e0
Martin Pagels <office@martinpagels.de>
2021-07-17 01:52:45 +0200

    Pass toggleStatusOfTodo to ToDoItem

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ToDoItem from "./ToDoItem";
 
-export default function ToDoList({ todos, deleteToDo }) {
+export default function ToDoList({ todos, deleteToDo, toggleStatusOfTodo }) {
   return (
     <ul>
       {todos.map((todo) => {
-        return <ToDoItem key={todo.todo} todo={todo} deleteToDo={deleteToDo} />;
+        return (
+          <ToDoItem
+            key={todo.todo}
+            todo={todo}
+            deleteToDo={deleteToDo}
+            toggleStatusOfTodo={toggleStatusOfTodo}
+          />
+        );
       })}
     </ul>
   );
