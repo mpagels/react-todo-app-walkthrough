05875b82a1a980cd7a4a320066bf8bb3840d6ae9
Martin Pagels <office@martinpagels.de>
2021-07-17 20:19:55 +0200

    Add editMode to ToDoItem and basic styling

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import editSymbol from "../assets/svg/edit.svg";
+export default function ToDo({
+  todo,
+  deleteToDo,
+  toggleStatusOfTodo,
 export default function ToDo({ todo, deleteToDo, toggleStatusOfTodo }) {
+}) {
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  function handleToggleEditeMode() {
+    setIsEditMode((previousState) => !previousState);
+    setInput(todo.todo);
+  }
   function handleOnDeleteClick() {
     deleteToDo(todo.todo);
   }
@@ -10,23 +21,40 @@ export default function ToDo({ todo, deleteToDo, toggleStatusOfTodo }) {
   }
   return (
     <li className="ToDo">
-      <div className="ToDo__btn--wrapper">
-        <button onClick={handleOnDeleteClick} className="ToDo__btn delete">
-          X
-        </button>
-        <button className="ToDo__btn--edit">
-          <img src={editSymbol} alt="" />
-        </button>
-      </div>
-      <p>{todo.todo}</p>
-      <button
-        onClick={handleOnToggleClick}
-        className={`ToDo__btn ${
-          todo.status === "Pending" ? "pending" : "done"
-        }`}
-      >
-        {todo.status}
-      </button>
+      {!isEditMode ? (
+        <>
+          <div className="ToDo__btn--wrapper">
+            <button onClick={handleOnDeleteClick} className="ToDo__btn delete">
+              X
+            </button>
+            <button onClick={handleToggleEditeMode} className="ToDo__btn--edit">
+              <img src={editSymbol} alt="" />
+            </button>
+          </div>
+          <p>{todo.todo}</p>
+          <button
+            onClick={handleOnToggleClick}
+            className={`ToDo__btn ${
+              todo.status === "Pending" ? "pending" : "done"
+            }`}
+          >
+            {todo.status}
+          </button>
+        </>
+      ) : (
+        <div className="ToDo__btn--wrapper">
+          <button onClick={handleToggleEditeMode} className="ToDo__btn delete">
+            X
+          </button>
+            <input
+              className="ToDo__Input"
+            />
+            <button type="submit" className="ToDo__btn">
+              Update
+            </button>
+          </form>
+        </div>
+      )}
     </li>
   );
 }
